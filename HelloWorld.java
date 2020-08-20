872448506ba8145450636a7083f6e075f7047ccd
JesseSal <23jesse.salinas@boiseschools.net>
2020-08-20 16:49:57 +0000

    Completed the Assignment

---
+++ HelloWorld.java
@@ -0,0 +1,48 @@
+public class HelloWorld{
+
+/*
+Jesse Salinas
+Period 3
+My ASCII art is of a double bass
+*/
+  public static void main(String args[]){
+System.out.println("░░░░░░░░░░░░██░░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░░██░░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░████░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░████░░░░░░░░░░");
+System.out.println("░░░░░░░░█──││││──█░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░█──││││──█░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░││││░░░░░░░░░░");
+System.out.println("░░░░░░░░░██││││██░░░░░░░░");
+System.out.println("░░░░░░░██▒▒││││▒▒██░░░░░░");
+System.out.println("░░░░░░█▒▒▒▒││││▒▒▒▒█░░░░░");
+System.out.println("░░░░░█▒▒▒▒▒││││▒▒▒▒▒█░░░░");
+System.out.println("░░░░░█▒▒▒▒▒││││▒▒▒▒▒█░░░░");
+System.out.println("░░░░░█▒▒▒▒▒││││▒▒▒▒▒█░░░░");
+System.out.println("░░░░░░█▒▒█▒││││▒█▒▒█░░░░░");
+System.out.println("░░░░░░░█▒▒█││││█▒▒█░░░░░░");
+System.out.println("░░░░░░░░█▒█││││█▒█░░░░░░░");
+System.out.println("░░░░░░██▒█░││││░█▒██░░░░░");
+System.out.println("░░░░░█▒▒▒█─┼┼┼┼─█▒▒▒█░░░░");
+System.out.println("░░░░█▒▒▒▒▒▒││││▒▒▒▒▒▒█░░░");
+System.out.println("░░░░█▒▒▒▒▒▒││││▒▒▒▒▒▒█░░░");
+System.out.println("░░░░█▒▒▒▒▒▒││││▒▒▒▒▒▒█░░░");
+System.out.println("░░░░░█▒▒▒▒▒████▒▒▒▒▒█░░░░");
+System.out.println("░░░░░░█▒▒▒▒▒██▒▒▒▒▒█░░░░░");
+System.out.println("░░░░░░░██▒▒▒██▒▒▒██░░░░░░");
+System.out.println("░░░░░░░░░████████░░░░░░░░");
+System.out.println("░░░░░░░░░░░░██░░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░░││░░░░░░░░░░░");
+System.out.println("░░░░░░░░░░░░██░░░░░░░░░░░");
+
+
+    
+  }
+
+}
